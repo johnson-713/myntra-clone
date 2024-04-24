3e3b07dee64a091e93341588d71311ca54f4b079
johnson-713 <johnsonelangovan@gmail.com>
2024-04-24 21:07:39 +0530

    category page completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { ShopCategory } from './Pages/ShopCategory'
 import { Login } from './Pages/Login'
 import { Cart } from './Pages/Cart'
 import { Footer } from './Components/Footer/Footer'
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
 
@@ -15,9 +18,9 @@ function App() {
     <Navbar />
       <Routes>
         <Route path='/' element={<Shop />} />
-        <Route path='/men' element={<ShopCategory category='men' />} />
-        <Route path='/women' element={<ShopCategory category='women' />} />
-        <Route path='/kids' element={<ShopCategory category='kids' />} />
+        <Route path='/men' element={<ShopCategory banner={men_banner} category='men' />} />
+        <Route path='/women' element={<ShopCategory banner={women_banner} category='women' />} />
+        <Route path='/kids' element={<ShopCategory banner={kids_banner} category='kid' />} />
         <Route path='/login' element={<Login />} />
         <Route path='/cart' element={<Cart />} />
       </Routes>
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,48 @@
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import "./ShopCategory.css";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import { Items } from "../Components/Items/Items";
 
-
-export const ShopCategory = () => {
+export const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
   return (
-    <div>ShopCategory</div>
-  )
-}
+    <div className="shop-category">
+      <img
+        src={props.banner}
+        alt="banner-image"
+        className="shop-category-banner"
+      />
+      <div className="shop-category-indexSort">
+        <p>
+          <span>
+            <strong>showing 1 -12</strong>
+          </span>{" "}
+          out of 36 products
+        </p>
+        <div className="shop-category-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shop-category-products">
+        {all_product?.map((i, ind) => {
+          if (props.category === i?.category) {
+            return (
+              <Items
+                key={ind}
+                image={i?.image}
+                name={i?.name}
+                new_price={i?.new_price}
+                old_price={i?.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <div className="shop-category-loadmore">Explore more</div>
+    </div>
+  );
+};
